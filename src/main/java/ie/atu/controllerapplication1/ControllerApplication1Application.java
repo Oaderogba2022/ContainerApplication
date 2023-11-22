2910a435dce23baa9d81844687c43127c1af7e60
Olamide Aderogba <G00401971@atu.ie>
2023-11-22 15:24:07 +0000

    Created Class called HelloController which will return hello to the User

---
+++ src/main/java/ie/atu/controllerapplication1/ControllerApplication1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.controllerapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerApplication1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/controllerapplication1/ControllerApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.controllerapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplication1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
